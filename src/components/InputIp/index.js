b2fe58ff945efc936583ac497daf405118f8b217
blazerius54 <79137837220@yandex.ru>
2018-11-23 00:56:18 +0700

    develop/ip validation, initial request

--- src/components/InputIp/index.js
+++ src/components/InputIp/index.js
@@ -22,14 +22,45 @@ class InputIp extends Component {
   constructor() {
     super();
     this.state = {
+      ipAddress: '',
     };
   }
+
+  validateIp = e => {
+    const regex = /^[0-9.\b]+$/;
+    const { value } = e.target;
+    if (value === '' || regex.test(value)) {
+      this.setIpAddress(value);
+    }
+  };
+
+  setIpAddress = ipAddress => {
+    console.log(ipAddress);
+    this.setState({
+      ipAddress,
+    });
+  };
+
+  sendRequest = () => {
+    
+
+    fetch(
+      `https://api.2ip.ua/geo.json?ip=8.8.8.8`,
+    )
+      .then(response => {
+        if (response.status !== 200) {
+          return;
+        }
+        return response;
+      })
+      .then(response => response.json().then(response => console.log(response)));
+  };
 
   render() {
     return (
       <InputWrapper>
-        <input type="text" placeholder="Введите ip-адрес"/>
-        <button>проверить</button>
+        <input type="text" placeholder="Введите ip-адрес" value={this.state.ipAddress} onChange={(e)=>this.validateIp(e)}/>
+        <button onClick={()=>this.sendRequest()}>проверить</button>
       </InputWrapper>
     );
   }
